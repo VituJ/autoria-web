fa580c17e59aabfbb2d4d7617308b9c75e9ccf64
VituJ <jvboliveira0405@gmail.com>
2022-11-08 08:44:40 -0300

    main

--- codigosjs.js
+++ codigosjs.js
@@ -4,13 +4,28 @@ function gravar(){
     var idadepessoa = 0;
     nomepessoa  = document.getElementById("nome").value;
     idadepessoa = document.getElementById("idade").value;
-    if (nomepessoa != "" && idadepessoa != 0){
+    if (nomepessoa != "" && idadepessoa > 0){
         divinfogravadas.innerHTML = `${nomepessoa + " tem " + idadepessoa + " anos"}`;
         contapessoas = contapessoas + 1;
         divcontapessoas.innerHTML = `${contapessoas + " foram contabilizadas"}`;
     }
+
+    else if (idadepessoa < 0){
+        window.alert("Não é aceito idades menores que 0");
+    }
+
+    else if (nomepessoa == "" && idadepessoa > 0){
+        window.alert("Falta escrever um nome");
+        document.getElementById('nome').focus();
+    }
+
+    else if (nomepessoa != "" && idadepessoa == ""){    
+        window.alert("Falta pôr uma idade");
+        document.getElementById('idade').focus();
+    }
+
     else{
-        window.alert("Faltou informar nome e/ou a idade")
+        window.alert("Faltou informar nome e/ou a idade");
     }
 
     document.getElementById('nome').value = '';
